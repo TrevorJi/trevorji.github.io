7a2780049741586d6cd54364d5c6b0476df1ca6d
trevorji <trevor.ll.ji@gmail.com>
2024-05-04 11:42:32 +0900

    push#23

--- js.js
+++ js.js
@@ -1,42 +1,26 @@
-function reorder() {
-    var obj = document.querySelectorAll('.center p');
-    var count = obj.length;
-    var itA;
-    var itB;
-
-    for(var i=1; i<=count; i++) {
-    itA = document.querySelector('.center p:nth-child('+i+')');
-    
-    rnd = rmax(count);
-    if(rnd==i) continue;
-    
-    itB = document.querySelector('.center p:nth-child('+rnd+')');
-    
-    var itAC = structuredClone(itA);
-    var itBC = structuredClone(itB);
-    
-    insertAfter(itBC, itA);
-    itA.remove();
-    
-    insertAfter(itAC, itB);
-    itB.remove();
-    }
-    }
-    
-    function rmax(max){
-    var n = Math.round(Math.random()*max);
-    return n == 0 ? 1 : n;
-    }
-
-    function insertAfter(newNode, existingNode) {
-        existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
-    }
+function shuffle(){
+    var list = document.querySelector('div.center');
+    for (var i = list.children.length; i >= 0; i--) {
+        list.appendChild(list.children[Math.random() * i | 0]);
+        }
+}
 
 function isShe() {
     var input = document.querySelector('#firstquestion');
     var answer = input.value.toLowerCase();
     if(answer == 'maimai'){
         document.getElementById('q1').style.display = "none";
+        document.getElementById('q2').style.display = "grid";
+    } else {
+        window.location.replace("/404.html");
+    }
+}
+
+function isMe() {
+    var input = document.querySelector('#secondquestion');
+    var answer = input.value.toLowerCase();
+    if(answer == 'talanvantus'){
+        window.location.replace("/welcome.html");
     } else {
         window.location.replace("/404.html");
     }
