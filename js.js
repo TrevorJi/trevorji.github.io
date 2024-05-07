eb3da1322e09d167fe6dc69d8c5a1ca8cc7bb61c
TrevorJi <trevor.ll.ji@gmail.com>
2024-05-07 12:55:21 +0900

    push #28

--- js.js
+++ js.js
@@ -5,21 +5,23 @@ function shuffle(){
         }
 }
 
-function isShe() {
+function isShe(event) {
     var input = document.querySelector('#firstquestion');
     var answer = input.value.toLowerCase();
-    if(answer == 'maimai'){
+    if(answer === 'maimai'){
         document.getElementById('q1').style.display = "none";
         document.getElementById('q2').style.display = "grid";
+        event.preventDefault()
     } else {
         window.location.replace("/404.html");
+        event.preventDefault()
     }
 }
 
 function isMe() {
     var input = document.querySelector('#secondquestion');
     var answer = input.value.toLowerCase();
-    if(answer == 'talanvantus'){
+    if(answer === 'talanvantus'){
         window.location.replace("/welcome.html");
     } else {
         window.location.replace("/404.html");
