d54651db6d0cf8fbd4ff975bc8382d96564bdde5
TrevorJi <trevor.ll.ji@gmail.com>
2024-05-10 15:38:51 +0900

    push #56

--- js.js
+++ js.js
@@ -76,7 +76,9 @@ function whatIsHisRealName(event) {
 
 function turnPages(path){
     var audio = new Audio('/resources/turning_pages.mp3');
-    var duration = audio.duration;
-    audio.play();
-    setTimeout(location.replace(path), duration*1000);
+    audio.onloadeddata = function(){
+        var duration = audio.duration;
+        audio.play();
+        setTimeout(location.replace(path), duration*1000 + 300);
+    };
 }
